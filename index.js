e299bd3f19b8b38df6137b6b8f377b2215279057
Christiane Drangle <positivitee@proton.me>
2023-04-21 17:19:38 -0400

    Arrow Functions passing

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (x) => x / 100
+
+// const square = (parameter1, parameter2) => square
+const add = (parameter1, parameter2) => parameter1 + parameter2
+add()
+
+// const squareMulipliesIt = (parameter1) => parameter1 * parameter1
+// parameter1 = (x) => x * x
+const square = (parameter1) => parameter1 * parameter1
